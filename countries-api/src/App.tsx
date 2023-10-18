31cc158ca23905306ed6e3d02d0dab3d1d7b1ad5
Felicia Lydén <felicia.lyden@gmail.com>
2023-10-18 20:43:33 +0200

    created vite project

---
+++ countries-api/src/App.tsx
@@ -0,0 +1,15 @@
+//import { useState } from 'react'
+import React from 'react'
+import './App.css'
+
+export function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+      </div>
+    </>
+  )
+}
+
