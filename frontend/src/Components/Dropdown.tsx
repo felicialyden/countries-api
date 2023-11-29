49b8bd82c2927aced4f76f737e12a499c48083a3
Felicia <felicia.lyden@gmail.com>
2023-11-29 19:04:53 +0100

    fix: incorrect value in filter select for americas

--- frontend/src/Components/Dropdown.tsx
+++ frontend/src/Components/Dropdown.tsx
@@ -13,8 +13,9 @@ export const Dropdown = ({ setRegion }: DropdownProps) => {
     return (
         <select onChange={handleFilterRegion} className='clickable align-right' name="regions" id="regions"> 
         <option value="all">Filter by region</option> 
+        <option value="all">All</option> 
         <option value="africa">Africa</option> 
-        <option value="america">America</option> 
+        <option value="americas">America</option> 
         <option value="asia">Asia</option> 
         <option value="europe">Europe</option> 
         <option value="oceania">Oceania</option> 
