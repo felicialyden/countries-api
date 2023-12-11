07aa3252859353b0d43a3829124493f097314176
Felicia <felicia.lyden@gmail.com>
2023-12-11 20:21:30 +0100

    style details page for desktop

--- frontend/src/pages/Detail.tsx
+++ frontend/src/pages/Detail.tsx
@@ -37,37 +37,69 @@ export const Detail = ({ countryData }: DetailProps) => {
         </svg>
         Back
       </button>
-      <img
-        className="flag-img"
-        src={country?.flag}
-        alt={`${country?.name}-flag`}
-      />
-      <h2 className="country-card-title">{country?.name}</h2>
-      <div className="country-info-wrapper">
-        <p><span className="bold-text">Native Name: </span> {country?.nativeName}</p>
-        <p><span className="bold-text">Population: </span> {country?.population.toLocaleString()}</p>
-        <p><span className="bold-text">Region: </span>{country?.region}</p>
-        <p><span className="bold-text">Sub Region: </span>{country?.subregion}</p>
-        <p><span className="bold-text">Capital: </span>{country?.capital}</p>
-      </div>
-      <div className="country-info-wrapper">
-        <p><span className="bold-text">Top Level Domain: </span>{country?.topLevelDomain[0]}</p>
-        <p><span className="bold-text">Currencies: </span>{country?.currencies[0].name}</p>
-        <p><span className="bold-text">Languages: </span>{country?.languages[0].name}</p>
-      </div>
-      <div className="country-info-wrapper">
-        {borderCountries.length > 0 && <h3>Border Countries:</h3>}
-        <ul className="border-country-list">
-          {borderCountries.map(({ alpha3Code, name }) => (
-            <li
-              onClick={() => navigate(`/${alpha3Code}`)}
-              key={alpha3Code}
-              className="border-country-list-item clickable"
-            >
-              {name}
-            </li>
-          ))}
-        </ul>
+      <div className="country-data">
+        <img
+          className="flag-img"
+          src={country?.flag}
+          alt={`${country?.name}-flag`}
+        />
+
+        <div className="country-details-wrapper">
+          <h2 className="country-card-title">{country?.name}</h2>
+          <div className="country-info-wrapper-wrapper">
+            <div className="country-info-wrapper">
+              <p>
+                <span className="bold-text">Native Name: </span>{" "}
+                {country?.nativeName}
+              </p>
+              <p>
+                <span className="bold-text">Population: </span>{" "}
+                {country?.population.toLocaleString()}
+              </p>
+              <p>
+                <span className="bold-text">Region: </span>
+                {country?.region}
+              </p>
+              <p>
+                <span className="bold-text">Sub Region: </span>
+                {country?.subregion}
+              </p>
+              <p>
+                <span className="bold-text">Capital: </span>
+                {country?.capital}
+              </p>
+            </div>
+            <div className="country-info-wrapper">
+              <p>
+                <span className="bold-text">Top Level Domain: </span>
+                {country?.topLevelDomain[0]}
+              </p>
+              <p>
+                <span className="bold-text">Currencies: </span>
+                {country?.currencies[0].name}
+              </p>
+              <p>
+                <span className="bold-text">Languages: </span>
+                {country?.languages[0].name}
+              </p>
+            </div>
+          </div>
+
+          <div className="country-info-wrapper">
+            {borderCountries.length > 0 && <h3>Border Countries:</h3>}
+            <ul className="border-country-list">
+              {borderCountries.map(({ alpha3Code, name }) => (
+                <li
+                  onClick={() => navigate(`/${alpha3Code}`)}
+                  key={alpha3Code}
+                  className="border-country-list-item clickable"
+                >
+                  {name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
